c87c7fc73da7e25e7dbc2ccdf143d01cab6616c5
mattsershaw <buddylfs@gmail.com>
2021-01-02 18:09:34 +0900

    useStateを使用

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { Fragment, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [text, setText] = useState('');
+
+  const changedText = (e) => {
+    setText(e.target.value);
+  }
+  const clickedButton = () => {
+    alert(text);
+  }
+
   return (
-    <h1>Hello React!</h1>  
+    <Fragment>
+      <h1>Hello React!</h1>
+      <input type="text" value={text} onChange={changedText} />
+      <br />
+      <button onClick={clickedButton}>Click Me!</button>
+    </Fragment>
   );
-}
+} // import Reactのところを{ Fragment }にしてちまちま必要なところだけを引っ張ってくる
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello React!</h1> {/* 要素が2つ以上だとエラー / divで囲むことが多い */}
+//       <input type="text" />
+//       <button>Click Me!</button>
+//     </div>
+//   );
+// }
 
 export default App;
