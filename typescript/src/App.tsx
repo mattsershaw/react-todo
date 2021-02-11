4c148a5a648c3ff5ab7826dbebc5014c57228ee0
mattsershaw <buddylfs@gmail.com>
2021-02-11 14:56:34 +0900

    axiosでTodo一覧取得まで

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Axios from 'axios';
 import { TodoList } from './views/TodoList/TodoList';
 import styles from './App.css';
+
+Axios.defaults.baseURL = 'http://localhost:4000/api/';
 
 export const App: React.FC = () => {
   return (
--- typescript/src/views/TodoList/TodoList.tsx
+++ typescript/src/views/TodoList/TodoList.tsx
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import { Todo } from '../../model/Todo';
 import { TodoItem } from '../../components/TodoItem';
 import styles from './TodoList.css';
 
-const todos: Todo[] = [
-  {
-    id: 0,
-    title: 'Title1aaaaaa',
-    description: 'Description1',
-  },
-  {
-    id: 1,
-    title: 'Title2vvv',
-    description: 'Description2',
-  },
-  {
-    id: 2,
-    title: 'Title3',
-    description: 'Description3',
-  },
-];
+// const todos: Todo[] = [
+//   {
+//     id: 0,
+//     title: 'Title1aaaaaa',
+//     description: 'Description1',
+//   },
+//   {
+//     id: 1,
+//     title: 'Title2vvv',
+//     description: 'Description2',
+//   },
+//   {
+//     id: 2,
+//     title: 'Title3',
+//     description: 'Description3',
+//   },
+// ];
 
 export const TodoList: React.FC = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const getTodoList = async () => {
+      const response = await Axios.get<Todo[]>('todos');
+      setTodoList(response.data);
+    };
+    getTodoList();
+  }, [setTodoList]);
+
   return (
     <React.Fragment>
       <div>
@@ -32,7 +43,7 @@ export const TodoList: React.FC = () => {
         <button className={styles.todoAddButton}>Click Me!!!</button>
       </div>
 
-      {todos.map((todo) => {
+      {todoList.map((todo) => {
         return <TodoItem key={todo.id} todo={todo} />;
         // todoを引数としてTodo[]を回す
       })}
